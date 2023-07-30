42dbd2824f47e9670d57aab1521126f1c9763ba8
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-07-31 01:55:08 +0530

    practical example of parsing the template string with Tagged Template Literal!

--- 2.Template-strings/app.js
+++ 2.Template-strings/app.js
@@ -39,3 +39,27 @@ function showStatement(stringArr, ...vars) {
   console.log(`Variable Array is:`);
   console.log(vars);
 }
+
+// Use Case of Tagged Template Literal: Let's add strong tag across the template literals in whatever way we want
+
+const author = "Emmei";
+const bookName = "The Vision";
+
+const quote = getStatement`The book by the name ${bookName} is written by ${author}`;
+
+// Let's create the Tagged function
+
+function getStatement(arr, ...variables) {
+  console.log(arr);
+  console.log(variables);
+
+  return arr
+    .map((string, index) => {
+      return `${string} <strong class="author">${
+        variables[index] || ""
+      }</strong>`;
+    })
+    .join("");
+}
+
+document.querySelector(".book-details").innerHTML = quote;
